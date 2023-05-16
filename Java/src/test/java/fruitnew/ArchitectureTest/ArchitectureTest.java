94bbb93730a81978500a301bb2ee43b5c5795aa9
ericmettt <eric.metternich@iav.de>
2023-05-16 18:29:58 +0200

    Added archunit architecture tests

---
+++ Java/src/test/java/fruitnew/ArchitectureTest/ArchitectureTest.java
@@ -0,0 +1,26 @@
+package fruitnew.ArchitectureTest;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+public class ArchitectureTest{
+
+    @Test
+    public void testLayeredArchitecture() {
+        JavaClasses importedClasses = new ClassFileImporter().withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS).importPackages("fruitnew");
+
+        ArchRule rule = layeredArchitecture().consideringAllDependencies()
+                .layer("Controller").definedBy("fruitnew.controller..")
+                .layer("Service").definedBy("fruitnew.service..")
+                .layer("Repository").definedBy("fruitnew.repository..")
+
+                .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+                .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
+                .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service");
+
+        rule.check(importedClasses);
+    }
+}
