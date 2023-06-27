46335e54c1abe4a9289adb8fb474c38a32cc6430
ericmettt <eric.metternich@iav.de>
2023-06-27 16:15:07 +0200

    updated frontend

--- frontend/sqs-fruit-frontend/src/components/Fruit.js
+++ frontend/sqs-fruit-frontend/src/components/Fruit.js
@@ -43,7 +43,7 @@ export default function Fruit() {
         }
       })
       .catch(error => {
-        setError("There is no such frui");
+        setError("There is no such frut");
         setResponse(null);
       });
   };
