740a67d73cc6e7d29f00c54197d2e56301e66392
ericmettt <eric.metternich@iav.de>
2023-06-29 17:01:54 +0200

    added service tests

--- src/test/java/fruitnew/unittests/FruitServiceUnitTest.java
+++ src/test/java/fruitnew/unittests/FruitServiceUnitTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,9 +20,14 @@ import reactor.core.publisher.Mono;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
 public class FruitServiceUnitTest {
 
+    @Mock
+    private FruitRepository fruitRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
     @Test
     public void testGetFruitInformation() {
         // Mock the WebClient response
@@ -27,5 +35,16 @@ public class FruitServiceUnitTest {
         //fruitService.getFruitInformation("Fruit");
 
     }
+    @Test
+    void testSaveDetails() {
 
+        Fruit apple = new Fruit("Apple");
+        entityManager.persist(apple);
+        entityManager.flush();
+        entityManager.clear();
+
+        // Assert
+        Fruit savedApple = entityManager.find(Fruit.class, apple.getId());
+        assertNotNull(savedApple, "The fruit should be saved");
+    }
 }
