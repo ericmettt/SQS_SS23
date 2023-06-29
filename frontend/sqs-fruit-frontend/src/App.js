a149f6cd02887b4cdf8421ba587ae96fb17eed4f
ericmettt <eric.metternich@iav.de>
2023-06-29 22:32:01 +0200

    Implemendet Owasp reccommendations

--- frontend/sqs-fruit-frontend/src/App.js
+++ frontend/sqs-fruit-frontend/src/App.js
@@ -2,10 +2,25 @@
 import './App.css';
 import Appbar from './components/Appbar';
 import Fruit from './components/Fruit';
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+  <meta
+    http-equiv="Content-Security-Policy"
+    content={`
+      default-src 'self' http://localhost:*;
+      script-src 'self';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' ;
+      media-src 'self' ;
+      frame-ancestors 'none';
+    `}
+  />
+</Helmet>
+
       <Appbar></Appbar>
       <Fruit></Fruit>
     </div>
--- src/main/java/fruitnew/FruitnewApplication.java
+++ src/main/java/fruitnew/FruitnewApplication.java
@@ -2,12 +2,27 @@ package fruitnew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FruitnewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FruitnewApplication.class, args);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> serverHeaderCustomizer() {
+		return serverFactory -> serverFactory.setServerHeader("");
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> contentTypeOptionsCustomizer() {
+		return serverFactory -> serverFactory.addInitializers(servletContext -> {
+			servletContext.setInitParameter("X-Content-Type-Options", "nosniff");
+		});
 	}
 
 }
